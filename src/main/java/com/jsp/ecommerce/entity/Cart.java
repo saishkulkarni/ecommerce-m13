8d18dd6db0a76829d4b76d3591d587bbecf1264c
saishkulkarni <saishkulkarni7@gmail.com>
2025-05-06 12:02:55 +0530

    16. Add to Cart Success

--- src/main/java/com/jsp/ecommerce/entity/Cart.java
+++ src/main/java/com/jsp/ecommerce/entity/Cart.java
@@ -1,30 +1,22 @@
 package com.jsp.ecommerce.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class Cart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(nullable = false)
-	private Long quantity;
-	@Column(nullable = false)
-	private Double price;
-	@ManyToOne
-	private Orders orders;
-	@ManyToOne
-	private Product product;
 
-	@ManyToOne
-	Cart cart;
+	@OneToOne
+	Customer customer;
+
 }
